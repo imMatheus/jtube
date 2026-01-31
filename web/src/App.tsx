31cfb129cd50d6fe459eb7fb014642bae8dd5b3c
imMatheus <matheusmendesbarata03@gmail.com>
2026-01-31 03:41:49 -0800

    fixed scroll issue between pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router";
 import { Header } from "./components/layout/Header";
 import { Sidebar } from "./components/layout/Sidebar";
 import { ChannelPage } from "./pages/channel/ChannelPage";
@@ -6,9 +7,20 @@ import { VideoPage } from "./components/video/VideoPage";
 import { ShortsPage } from "./pages/shorts/ShortsPage";
 import { NotFoundPage } from "./components/NotFoundPage";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="min-h-screen bg-(--color-bg-primary) text-(--color-text-primary)">
+      <ScrollToTop />
       <Header />
       <Sidebar />
       <Routes>
